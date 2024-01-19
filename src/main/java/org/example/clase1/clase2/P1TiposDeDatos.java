12b7df4efff4bff22a141f36e248a5edcbda2bdf
AneeMar <analiamarilina@gmail.com>
2024-01-19 18:07:42 -0300

    commit 2

---
+++ src/main/java/org/example/clase1/clase2/P1TiposDeDatos.java
@@ -0,0 +1,13 @@
+package org.example.clase1.clase2;
+
+public class P1TiposDeDatos {
+    public static void main(String[] args) {
+        int var1 = 400000;
+        long var2= 400000000000L;
+        float var3= 12.55F;
+        double var4 = 13.250000000000000000000000000;
+        char var5 = '8'; //es un chart, no un numero o un string
+        boolean var6 = true;
+        
+    }
+}
--- src/main/java/org/example/clase1/clase2/PODesclaracioneInicializacion.java
+++ src/main/java/org/example/clase1/clase2/PODesclaracioneInicializacion.java
@@ -8,5 +8,11 @@ public class PODesclaracioneInicializacion {
 
         System.out.println("Variable "+ count);
 
+        //declaración e inicialización.
+
+        float precio = 140.59f; //hay que castearlo porque en defecto es un double
+        float precio2 = (float) 156.30; //otra forma de castearlo
+
+        System.out.println( "primer precio " + precio + "segundo precio " + precio2);
     }
 }
