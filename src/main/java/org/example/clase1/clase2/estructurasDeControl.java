30b29f1cd8b17c652170a59edb6d1a217c39d05d
AneeMar <analiamarilina@gmail.com>
2024-01-20 20:17:08 -0300

    Estructuras de control
    if, else-if, switch

---
+++ src/main/java/org/example/clase1/clase2/estructurasDeControl.java
@@ -0,0 +1,62 @@
+package org.example.clase1.clase2;
+
+public class estructurasDeControl {
+    public static void main(String[] args) {
+        int var1=100;
+        int var2=200;
+
+        if (var1 < var2) {
+            System.out.println("es menor");
+        }
+
+        int var3=300;
+        int var4=400;
+
+        if (var3 > var4) {
+            System.out.println("es menor");
+        } else {
+            System.out.println("es mayor");
+        }
+
+
+        //En este caso, el swich y el if else son equivalentes
+
+        
+        String diaDeLaSemana = "Domingo";
+
+        switch (diaDeLaSemana){
+
+            case "Lunes":
+                System.out.println("Hoy es Lunes");
+                break;
+            case "Martes":
+                System.out.println("Hoy es Martes");
+                break;
+            case "Miercoles":
+                System.out.println("Hoy es Miercoles");
+                break;
+            case "Jueves":
+                System.out.println("Hoy es Jueves");
+                break;
+            case "Viernes":
+                System.out.println("Hoy es Viernes");
+                break;
+            default:
+                System.out.println("Buen Fin de semana!");
+        }
+
+        if (diaDeLaSemana == "Lunes") {
+            System.out.println("Hoy es Lunes");
+        } else if (diaDeLaSemana == "Martes"){
+            System.out.println("Hoy es Martes");
+        } else if (diaDeLaSemana == "Miercoles"){
+            System.out.println("Hoy es Miercoles");
+        } else if (diaDeLaSemana == "Jueves") {
+            System.out.println("Hoy es Jueves");
+        } else if (diaDeLaSemana == "Viernes") {
+            System.out.println("Hoy es Viernes");
+        } else {
+            System.out.println("Buen fin de semana!");
+        }
+    }
+}
