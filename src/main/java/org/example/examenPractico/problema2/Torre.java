b70cc90d4b89da4206984f88895a3299acd6820f
AneeMar <analiamarilina@gmail.com>
2024-01-28 18:10:26 -0300

    terminando el practico 2

--- src/main/java/org/example/examenPractico/problema2/Torre.java
+++ src/main/java/org/example/examenPractico/problema2/Torre.java
@@ -3,12 +3,15 @@ package org.example.examenPractico.problema2;
 public class Torre extends Personaje{
 
     private Integer Nivel;
+
+    //constructor
     public Torre(Integer Salud, Integer Nivel) {
         super(Salud);
 
         this.Nivel = Nivel;
     }
 
+    //getter and setter
     public Integer getNivel() {
         return Nivel;
     }
@@ -16,4 +19,20 @@ public class Torre extends Personaje{
     public void setNivel(Integer nivel) {
         Nivel = nivel;
     }
+
+    //Metodo Entrenar
+
+    @Override
+    public void entrenar (){
+        setNivel(Nivel+1);
+        System.out.println("Excelente entrenamiento! Usted ahora es nivel: " + getNivel());
+    }
+
+    //instancia
+
+    public static void main(String[] args) {
+        Torre LT = new Torre(50,1);
+        LT.entrenar();
+    }
+
 }
